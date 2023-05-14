76071081ecbe5e75aaba4ba8d42d4a6a9e32b433
Raghav <kumarraghav71950@gmail.com>
2023-05-14 12:31:17 +0530

    adding models

--- models/Model.js
+++ models/Model.js
@@ -29,6 +29,13 @@ const studentSchema = new mongoose.Schema({
     },
     //Add Child Referencing For Subjects Here. 
     // subjects: []
+    subjects: [{
+
+        type: mongoose.Schema.Types.ObjectId,
+
+        ref: 'Subject'
+
+    }]
 });
 
 // Create the models
